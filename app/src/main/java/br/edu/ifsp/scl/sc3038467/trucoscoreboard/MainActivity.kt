4d4a6045b530126187f8e088bdb0242901caa364
Murilo Corrêa <murilosao06@gmail.com>
2026-08-17 16:14:14 -0300

    feat: add estrutura da MainActivity com ViewBinding

--- app/src/main/java/br/edu/ifsp/scl/sc3038467/trucoscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038467/trucoscoreboard/MainActivity.kt
@@ -5,16 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.sc3038467.trucoscoreboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+    private lateinit var binding: ActivityMainBinding
+    private var pontosA = 0
+    private var pontosB = 0
+
+    private var alertaMao11A = false
+    private var alertaMao11B = false
 }
